53d482ed2fd8a3c26af1d8d1d69e9f400ed31dc3
Justin Kephart <justinpianist1@yahoo.com>
2019-02-22 21:52:22 -0800

    Made Player Movement
    Player's Torso Rotates to Face Mouse
    Player's Legs Rotate to Velocity
    Option to always keep legs 90 degrees towards target.

---
+++ Season's Beatings/Assets/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController instance;
+    [SerializeField] GameObject PlayerTorso;
+    [SerializeField] GameObject PlayerLegs;
+    Rigidbody2D rigidbody2D;
+
+    [SerializeField] float speed = 5f;
+    [SerializeField] bool faceTarget = true;
+
+    void Awake()
+    {
+        instance = this;
+        rigidbody2D = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerTorso.transform.up = (Vector2) (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
+        rigidbody2D.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed);
+        if (rigidbody2D.velocity != Vector2.zero)
+        {
+            PlayerLegs.transform.up = rigidbody2D.velocity;
+            if (faceTarget && Quaternion.Angle(PlayerTorso.transform.rotation, PlayerLegs.transform.rotation) > 90) PlayerLegs.transform.up = -1 * PlayerLegs.transform.up; // Keeps body facing mouse
+        }
+
+        //DEBUG
+        Debug.DrawRay(transform.position, PlayerTorso.transform.up, Color.red, .01f);
+        Debug.DrawRay(transform.position, PlayerLegs.transform.up, Color.green, .01f); 
+    }
+}
