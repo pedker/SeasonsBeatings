26af69affacfaaae4c05ef5766800e9706e42018
JosefBertolini <39634656+JosefBertolini@users.noreply.github.com>
2019-02-23 18:16:43 -0800

    Made Scripts folder, created GameManager script with singleton patter, moved PlayerController.cs into scripts folder, also renamed rigidbody2D variable to m_rigidbody2D to get rid of a warning

---
+++ Season's Beatings/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance { get; private set; }
+    public int score = 0;
+
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+
+        //SceneManager.sceneLoaded += ????; //Use this in vain of Start()
+    }
+
+
+}
