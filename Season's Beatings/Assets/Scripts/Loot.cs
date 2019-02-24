c9de2aec6d871e9370f1af1d124c947bb0dd0b86
Josef Bertolini <bertolij@uci.edu>
2019-02-24 15:03:18 -0800

    Created Loot script and I_item interface, also edited player controller to add onCollisionEnter2D

---
+++ Season's Beatings/Assets/Scripts/Loot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loot : MonoBehaviour, I_item
+{
+    GameManager gameManager;
+    [SerializeField] int m_score = 0;
+
+    void Start()
+    {
+        gameManager = GameManager.instance;
+    }
+
+    public void pickUp()
+    {
+        Destroy(this.gameObject);
+        gameManager.score += m_score;
+    }
+}
