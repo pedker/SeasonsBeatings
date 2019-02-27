55ba90019c82e965a35a2a9077e9d1bd0f2b0da6
Justin Kephart <justinpianist1@yahoo.com>
2019-02-26 18:42:08 -0800

    UI Fix Attempt

--- Season's Beatings/Assets/Scripts/UIManager.cs
+++ Season's Beatings/Assets/Scripts/UIManager.cs
@@ -6,31 +6,29 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    public GameObject pauseMenu;
+    public GameObject pauseMenu; 
     public GameObject gameCanvas;
-    public GameObject mainMenuCanvas;
-    private GameManager gameManager;
+    public GameObject mainMenuCanvas; // Useless
+    private GameManager gameManager; //Not used. Useless
     bool isPaused = false;
     bool inGame = false;
-    bool atMainMenu = true;
-
-    // Start is called before the first frame update
+    bool atMainMenu = true; // Useless
+    
     void Start()
     {
-        gameManager = GameManager.instance;
+        gameManager = GameManager.instance;  //Not used. Useless
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             PauseControl();
-            GameUIControl();
+            GameUIControl();    // Only needed if disabling the UI is really necessary
         }
     }
 
-    public void MainMenuUIControl()
+    public void MainMenuUIControl() // Useless function. Just disable the ui in the main menu scene.
     {
         atMainMenu = !atMainMenu;
         if (atMainMenu)
@@ -39,7 +37,7 @@ public class UIManager : MonoBehaviour
             mainMenuCanvas.SetActive(false);
     }
 
-    public void PauseControl()
+    public void PauseControl() // Now this should be disabled in the main menu scene
     {
         isPaused = !isPaused;
         if( isPaused )
@@ -62,13 +60,14 @@ public class UIManager : MonoBehaviour
         else
             gameCanvas.SetActive(false);
     }
-    public void LoadScene( string sceneName )
+
+    public void LoadScene( string sceneName ) // Useless function. Just disable the ui in the main menu scene.
     {
-        if( sceneName == "Main Menu" )
+        //if( sceneName == "Main Menu" )    
         {
-            MainMenuUIControl();
-            GameUIControl();
-            PauseControl();
+           // MainMenuUIControl();
+            //GameUIControl();
+           // PauseControl();
         }
         SceneManager.LoadScene(sceneName);
     }
