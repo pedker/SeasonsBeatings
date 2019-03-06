0892b6232de531594a453ae54bdcc1eedbfb0a85
Parker Levin <pedker@gmail.com>
2019-03-06 02:52:02 -0800

    Removed audio source components that shouldn't be there. trying to figure out spacial audio

--- Season's Beatings/Assets/Scripts/AudioChannel.cs
+++ Season's Beatings/Assets/Scripts/AudioChannel.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class AudioChannel : MonoBehaviour
 {
+    [SerializeField] float soundRange = 10.0f;
     private AudioSource m_audioPlayer;
     public bool inUse = false;
 
     void Awake()
     {
         m_audioPlayer = this.GetComponent<AudioSource>();
+        m_audioPlayer.maxDistance = soundRange;
         inUse = false;
     }
 
