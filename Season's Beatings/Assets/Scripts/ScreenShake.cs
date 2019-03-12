de9dc7cdf961a093235fa3cbf0fd1f78cd9fc04f
Gabe Eck <eck.gabe@gmail.com>
2019-03-12 16:52:09 -0700

    Adding screen shake

---
+++ Season's Beatings/Assets/Scripts/ScreenShake.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShake : MonoBehaviour
+{
+    IEnumerator Shake (float duration, float magnitude)
+    {
+        Vector3 originalPos = transform.localPosition;
+
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            transform.localPosition = new Vector3(x, y, originalPos.z);
+        }
+    }
+}
