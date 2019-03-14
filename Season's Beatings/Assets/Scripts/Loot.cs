fac95a7e61c48aea2ff21a35d8b66412c565623e
Justin Kephart <justinpianist1@yahoo.com>
2019-03-13 19:45:15 -0700

    Animated loot

--- Season's Beatings/Assets/Scripts/Loot.cs
+++ Season's Beatings/Assets/Scripts/Loot.cs
@@ -5,6 +5,22 @@ using UnityEngine;
 public class Loot : MonoBehaviour, IPickupable
 {
     [SerializeField] int m_score = 0;
+    [SerializeField] float animSpeed = 2;
+    [SerializeField] float animRange = .125f;
+
+    Vector3 startPosition;
+    Vector3 offset;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        offset = new Vector3(0, Mathf.Sin(Time.time * animSpeed) * animRange, 0);
+        transform.position = startPosition + offset;
+    }
 
     public void pickUp()
     {
