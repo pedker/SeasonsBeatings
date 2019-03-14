b41c3efa889d5cc64536c397ec92f34e249538a1
Gabe Eck <eck.gabe@gmail.com>
2019-03-14 00:34:24 -0700

    Timer fix and game over text changed

--- Season's Beatings/Assets/Scripts/GameManager.cs
+++ Season's Beatings/Assets/Scripts/GameManager.cs
@@ -14,12 +14,7 @@ public class GameManager : MonoBehaviour
 
     
     public int score { get; private set; } = 0;
-
-    [SerializeField] TextMeshProUGUI timerText = null;
-    public float timeLeft = 1200f;
-    private float timeMin = 0f;
-    private float timeSec = 0f;
-
+    
     [SerializeField] string pointsPlusFileName = null;
     [SerializeField] float pointsPlusVolume = 0.65f;
     [SerializeField] float pointsPlusPitchMinimum = 0.95f;
@@ -38,30 +33,7 @@ public class GameManager : MonoBehaviour
     {
         Time.timeScale = 1;
     }
-
-    private void Update()
-    {
-        UpdateTimer();
-        
-    }
-
-    void UpdateTimer()
-    {
-        if (timeLeft > 0)
-            timeLeft -= Time.deltaTime;
-        else
-            timeLeft = 0;
-        timeMin = timeLeft / 60;
-        timeSec = timeLeft % 60;
-        if (timeSec <= 10)
-        {
-            timerText.text = string.Format("{0}:0{1}", (int)timeMin, (int)timeSec);
-        }
-        else
-        {
-            timerText.text = string.Format("{0}:{1}", (int)timeMin, (int)timeSec);
-        }
-    }
+    
 
     public void addScore(int score)
     {
--- Season's Beatings/Assets/Scripts/UIManager.cs
+++ Season's Beatings/Assets/Scripts/UIManager.cs
@@ -15,6 +15,10 @@ public class UIManager : MonoBehaviour
     [SerializeField] Sprite defaultWeapon = null;
     [SerializeField] TextMeshProUGUI weaponDurability = null;
 
+    [SerializeField] TextMeshProUGUI timerText = null;
+    public float timeLeft = 300f;
+    private float timeMin = 0f;
+    private float timeSec = 0f;
 
     [SerializeField] GameObject pauseMenu = null;
 
@@ -32,6 +36,8 @@ public class UIManager : MonoBehaviour
 
     void Update()
     {
+        UpdateTimer();
+
         scoreText.text = "$" + GameManager.instance.score;
 
         if (PlayerController.instance.weapon is HandToHand)
@@ -47,11 +53,29 @@ public class UIManager : MonoBehaviour
 
         scoreText.text = "$" + GameManager.instance.score;
 
-        if (GameManager.instance.timeLeft == 0) EndGame(false);
+        if (timeLeft == 0) EndGame(false);
 
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             PauseControl();
+        }
+    }
+
+    void UpdateTimer()
+    {
+        if (timeLeft > 0)
+            timeLeft -= Time.deltaTime;
+        else
+            timeLeft = 0;
+        timeMin = timeLeft / 60;
+        timeSec = timeLeft % 60;
+        if (timeSec <= 10)
+        {
+            timerText.text = string.Format("{0}:0{1}", (int)timeMin, (int)timeSec);
+        }
+        else
+        {
+            timerText.text = string.Format("{0}:{1}", (int)timeMin, (int)timeSec);
         }
     }
 
@@ -87,15 +111,24 @@ public class UIManager : MonoBehaviour
         {
             message.text = "YOU SURVIVED";
             message.color = Color.green;
+            endPrice.text = "You Paid: $" + Mathf.Round(score * .25f * 100f) / 100f;
         }
-        else 
+        else if (timeLeft == 0)
+        {
+            message.text = "TIME'S UP";
+            message.color = Color.red;
+            endPrice.text = "";
+        }
+        else
         {
             message.text = "YOU DIED";
             message.color = Color.red;
+            endPrice.text = "";
+
         }
 
         endScore.text = "Total Value of Your Cart: $" + score;
-        endPrice.text = "You Paid: $" + Mathf.Round(score * .25f * 100f) / 100f;
+
 
         Destroy(this);
 
